d105d7b0ab8b3b5d2bb8ccc83e86121e4738138c
EmersonTeles <emersonteles21@gmail.com>
2023-06-12 01:48:40 -0300

    test: shipping to belo horizonte with 101kg

--- cypress/e2e/BestTransports.cy.ts
+++ cypress/e2e/BestTransports.cy.ts
@@ -9,6 +9,7 @@ describe('Get Best Transport', () => {
     cy.get('.v-btn__content').click()
     cy.contains('div', 'Insira os valores para realizar a análise.')
   })
+
   it('Shipping to Sao Paulo with 50kg', () => {
     cy.visit('http://127.0.0.1:5173/')
     cy.get(
@@ -16,6 +17,16 @@ describe('Get Best Transport', () => {
     ).click()
     cy.get('.v-list-item:nth-child(1)').click()
     cy.get('#input-3').type('50')
+    cy.get('.v-btn__content').click()
+  })
+
+  it('Shipping to Belo Horizonte with 101kg', () => {
+    cy.visit('http://127.0.0.1:5173/')
+    cy.get(
+      ':nth-child(1) > .v-input > .v-input__control > .v-field > .v-field__field > .v-field__input'
+    ).click()
+    cy.get('.v-list-item:nth-child(2)').click()
+    cy.get('#input-3').type('101')
     cy.get('.v-btn__content').click()
   })
 })
